0bf8cd1861c5a8e97dfdc64f5a11a486f8d29808
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-21 15:54:40 +0100

    Add a script for running the application for a given language and seeing the changes in the lexicon and the output

---
+++ run.sh
@@ -0,0 +1,8 @@
+language_code="$1"
+clear
+env/bin/mypy --strict src && \
+env/bin/python src/generate_affix_lexicon.py "$language_code" || exit
+cd spa && \
+../CompileFST/CompileFST.sh src/Segmentation.foma
+cd ..
+git diff "$language_code/src/Affixes.lexc" "$language_code/output.txt"
